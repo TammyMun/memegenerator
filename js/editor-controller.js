fffc1fa01dfb3b49760366eb82a1e2f395a7e2ed
Taz <tammylimited@gmail.com>
2019-06-06 12:46:08 +0300

    added text without render imgs

--- js/editor-controller.js
+++ js/editor-controller.js
@@ -1,15 +1,31 @@
 'use-strict';
 
-console.log('tom connected');
-
 let gCanvas;
 let gCtx;
+let gText;
 
+function drawText() {
+    gCtx.fillStyle = "white";
+    gCtx.textBaseline = 'middle';
+    gCtx.font = "50px 'Impact'";
+    gCtx.fillText(gText, 50, 50);
+    gCtx.stroke();
+}
+
+function renderText() {
+    document.getElementById('text-editor').addEventListener('keyup', function () {
+        gText = this.value;
+        drawText();
+        gCtx.fillText(gText, 50, 50);
+    })
+}
 
 function onInit() {
     gCanvas = document.getElementById('canvas');
     gCtx = canvas.getContext('2d');
     renderCanvas(gImgs[0].url);
+    drawText();
+    renderText();
 }
 
 
